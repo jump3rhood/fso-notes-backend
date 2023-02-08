7bfce98a1213da6ff6db25eac7ff2080cb5e5979
jump3rhood <mjm13121@gmail.com>
2023-02-08 16:35:00 +0530

    Notes app with get,post and delete routes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const http = require('http')
+const { response } = require('express')
+const express = require('express')
+const app = express()
+app.use(express.json())
 let notes = [
   {
     id: 1,
@@ -16,11 +19,50 @@ let notes = [
     important: true
   }
 ]
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+app.get('/', (req,res)=>{
+  res.send('hello')
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on PORT ${PORT}`)
+//app.method(PATH,HANDLER);
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find(note => note.id === id)
+  if(note)
+    res.json(note)
+  else
+    res.status(404).end()
+})
+
+app.post('/api/notes', (req, res)=> {
+  const body = req.body;
+  if(!body.content){
+    // 400 -bad request
+    return res.status(400).json({
+      error: 'Content missing'
+    })
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId()
+  }  
+  notes = notes.concat(note)
+  res.json(note)
+})
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+  
+  res.status(204).end()
+})
+const PORT = 3001;
+app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`))
